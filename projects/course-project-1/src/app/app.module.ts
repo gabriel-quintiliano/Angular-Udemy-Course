b3dc301ec0d700b70bc66b4169193d99e5726f2f
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-10-29 18:22:41 -0300

    Set up app.module.ts within project course-project-1 to use elements defined within modules recipe-book and shopping-list

--- projects/course-project-1/src/app/app.module.ts
+++ projects/course-project-1/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RecipeBookModule } from './modules/recipe-book/recipe-book.module';
+import { ShoppingListModule } from './modules/shopping-list/shopping-list.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent
+	],
+	imports: [
+		BrowserModule,
+		RecipeBookModule,
+		ShoppingListModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
