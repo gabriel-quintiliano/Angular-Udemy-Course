d25b35e7dd70c6b4e3a805d12228eb1aec8b9c25
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-10-29 13:52:07 -0300

    Complete task 3 - Practicing Directives

--- projects/task-3-practicing-directives/src/app/app.component.ts
+++ projects/task-3-practicing-directives/src/app/app.component.ts
@@ -1,9 +1,27 @@
+import { Block } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	showParagraph = false;
+	clickTimestamps: string[] = [];
+
+	// For use inside ngStyle
+	spanDisplayType = 'block';
+
+
+	onDisplayButtonClick() {
+
+		this.clickTimestamps.push((new Date()).toString())
+
+		if (this.showParagraph) {
+			this.showParagraph = false;
+		} else {
+			this.showParagraph = true;
+		}
+	}
 }
