a174825751ca0b6d8de5b0270f13208f40d416f6
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-10-29 17:54:57 -0300

    Create and set up shopping-list module within project course-project-1 manually (for better learning the overall structure)

--- projects/course-project-1/src/app/modules/recipe-book/recipe-book.module.ts
+++ projects/course-project-1/src/app/modules/recipe-book/recipe-book.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './components/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
-
-
 
 @NgModule({
 	declarations: [
@@ -21,4 +20,5 @@ import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.
 		RecipeDetailComponent
 	]
 })
+
 export class RecipeBookModule { }
