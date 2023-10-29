c06008a15fa721a2622f589427aa329f3aed5520
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-10-29 17:26:28 -0300

    Create and set up recipe-book module within project course-project-1 via AngularCLI

---
+++ projects/course-project-1/src/app/modules/recipe-book/recipe-book.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
+
+
+
+@NgModule({
+	declarations: [
+		RecipeListComponent,
+		RecipeItemComponent,
+		RecipeDetailComponent
+	],
+	imports: [
+		CommonModule
+	],
+	exports: [
+		RecipeListComponent,
+		RecipeItemComponent,
+		RecipeDetailComponent
+	]
+})
+export class RecipeBookModule { }
