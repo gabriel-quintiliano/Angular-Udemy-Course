8b400986d863d031783f0a3720043b7159e5d52b
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-10-30 23:13:54 -0300

    Implement recipe-list component within project course-project-1

--- projects/course-project-1/src/app/modules/recipe-book/components/recipe-list/recipe-list.component.ts
+++ projects/course-project-1/src/app/modules/recipe-book/components/recipe-list/recipe-list.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../../models/recipe.model';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+	selector: 'app-recipe-list',
+	templateUrl: './recipe-list.component.html',
+	styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+	recipes: Recipe[] = [
+		new Recipe('Batatas Hasselback', 'Batatas frtas e recheadas deliciosas', 'https://www.cookipedia.co.uk/wiki/images/8/8f/Hasselback_potatoes_recipe.jpg')
+	]
 }
