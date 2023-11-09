46ebe26cf87652fef2b5623a0ba5dba0810c7c89
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-11-08 22:56:05 -0300

    Implement recipe-item and refactor recipe-list components within project recipe-book-course-project

--- projects/recipe-book-course-project/src/app/modules/recipe-book/components/recipe-item/recipe-item.component.ts
+++ projects/recipe-book-course-project/src/app/modules/recipe-book/components/recipe-item/recipe-item.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recipe } from '../../models/recipe.model';
 
 @Component({
-  selector: 'app-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+	selector: 'app-recipe-item',
+	templateUrl: './recipe-item.component.html',
+	styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent {
+	@Input({ alias: 'recipeInfo', required: true }) recipe!: Recipe;
 
 }
