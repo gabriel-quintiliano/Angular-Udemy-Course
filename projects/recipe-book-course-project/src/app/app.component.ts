17df1218ae712de6f20a15203fc83de2223c8fa0
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-11-08 22:31:18 -0300

    Implement initial logic to display content alternatively as menu button is clicked within project recipe-book-course-project

--- projects/recipe-book-course-project/src/app/app.component.ts
+++ projects/recipe-book-course-project/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { ContentAvailableToBeShownType, contentAvailableToBeShown } from './models/content.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'course-project-I';
+	loadedFeature!: ContentAvailableToBeShownType;
+	contentAvailableToBeShown = contentAvailableToBeShown; // necessary as I want to use this enum like object in template
+
+
+	onUpdateContentRequest(contentName: ContentAvailableToBeShownType) {
+		this.loadedFeature = contentName;
+	}
 }
--- projects/recipe-book-course-project/src/app/components/header/header.component.ts
+++ projects/recipe-book-course-project/src/app/components/header/header.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ContentAvailableToBeShownType, contentAvailableToBeShown } from '../../models/content.model';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+	@Output() updateContentRequest = new EventEmitter<ContentAvailableToBeShownType>();
+	contentAvailableToBeShown = contentAvailableToBeShown; // necessary as I want to use this enum like object in template
 
+	onContentAnchorClick(contentName: ContentAvailableToBeShownType) {
+		this.updateContentRequest.emit(contentName);
+	}
 }
