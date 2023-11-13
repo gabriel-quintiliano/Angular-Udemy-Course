aee157d62b781acc718013eacffebe1fa3cfc309
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-11-12 21:42:39 -0300

    Implement sendNewIngredientInfo output in shopping-list-edit component's ts file within project recipe-book-course-project

--- projects/recipe-book-course-project/src/app/modules/shopping-list/components/shopping-list-edit/shopping-list-edit.component.ts
+++ projects/recipe-book-course-project/src/app/modules/shopping-list/components/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, ViewChild } from "@angular/core";
-import { unitsOfMeasure } from "../../models/ingredient.model";
+import { Component, ElementRef, Output, ViewChild, EventEmitter } from "@angular/core";
+import { Ingredient, UnitOfMeasureUnion, unitsOfMeasure } from "../../models/ingredient.model";
 
 
 @Component({
@@ -11,7 +11,23 @@ import { unitsOfMeasure } from "../../models/ingredient.model";
 export class ShoppingListEditComponent {
 	unitsOfMeasure = unitsOfMeasure; // so that I can use this imported object in the component's template
 
+	@Output('ingredientAdded') sendNewIngredientInfo = new EventEmitter<Ingredient>();
+
 	@ViewChild('nameInput') nameInputElem!: ElementRef<HTMLInputElement>;
 	@ViewChild('amountInput') amountInputElem!: ElementRef<HTMLInputElement>;
 	@ViewChild('unitOfMeasureSelect') unitOfMeasureSelectElem!: ElementRef<HTMLSelectElement>;
+
+	onIngredientAdded() {
+
+		const newIngredient: Ingredient = {
+			name: this.nameInputElem.nativeElement.value,
+			amount: Number(this.amountInputElem.nativeElement.value),
+			unitOfMeasure: this.unitOfMeasureSelectElem.nativeElement.selectedOptions[0].textContent! as UnitOfMeasureUnion,
+		};
+
+		this.sendNewIngredientInfo.emit(newIngredient)
+
+		this.nameInputElem.nativeElement.value = ""; // reset name input
+		this.amountInputElem.nativeElement.value = ""; // reset amount input
+	}
 }
