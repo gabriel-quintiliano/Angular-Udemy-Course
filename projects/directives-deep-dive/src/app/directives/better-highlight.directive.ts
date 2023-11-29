12545f9832412f0a59862ced0872ddb0054efa1b
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-11-28 23:10:02 -0300

    Refactor directive betterHighlight within project directives-deep-dive to use @HostListener and became reactive

--- projects/directives-deep-dive/src/app/directives/better-highlight.directive.ts
+++ projects/directives-deep-dive/src/app/directives/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, ElementRef } from '@angular/core';
+import { Directive, Renderer2, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
 	selector: '[appBetterHighlight]'
@@ -9,6 +9,15 @@ export class BetterHighlightDirective {
 
 	ngOnInit() {
 		// the forth arg for setStyle method are optional flags like '!important', among others
+		//this.renderer.setStyle(this.elemRef.nativeElement, "background-color", "lightblue")
+		// before having set @HostListener, I used this to style the element
+	}
+
+	@HostListener('mouseenter') onMouseOver() {
 		this.renderer.setStyle(this.elemRef.nativeElement, "background-color", "lightblue")
+	}
+
+	@HostListener('mouseleave') onMouseLeave() {
+		this.renderer.setStyle(this.elemRef.nativeElement, "background-color", "transparent")
 	}
 }
