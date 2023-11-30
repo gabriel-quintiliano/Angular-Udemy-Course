162017effff5247f25a29c0934f30ddb57dd4b1c
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-11-29 22:49:37 -0300

    Refactor directive betterHighlight within project directives-deep-dive to use @Input and implemented this on component app

--- projects/directives-deep-dive/src/app/directives/better-highlight.directive.ts
+++ projects/directives-deep-dive/src/app/directives/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, Renderer2, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
 	selector: '[appBetterHighlight]'
@@ -7,23 +7,30 @@ export class BetterHighlightDirective {
 
 	constructor(private elemRef: ElementRef, private renderer: Renderer2) { }
 
-	@HostBinding('style.backgroundColor') bgColor: string = "lightblue";
+	// if you set the name of the selector of the directive as an alias to some @Input
+	// you'll be able to use the directive in the element and set the input value all at once
+	@Input() defaultColor: string = 'lightblue';
+	@Input('appBetterHighlight') highlightColor: string = 'yellow';
+
+	@HostBinding('style.backgroundColor') bgColor!: string; // it will have a value assigned on ngOnInit for sure.
 
 	ngOnInit() {
 		// the forth arg for setStyle method are optional flags like '!important', among others
 		//this.renderer.setStyle(this.elemRef.nativeElement, "background-color", "lightblue")
 		// before having set @HostListener, I used this to style the element
+
+		this.bgColor = this.defaultColor // to make sure there is a value to be assigned (after @Inputs are initialized)
 	}
 
 	@HostListener('mouseenter') onMouseOver() {
 		// background color change via Renderer2 just like @HostBinding('style.backgroundColor') allows us
 		// this.renderer.setStyle(this.elemRef.nativeElement, "background-color", "transparent")
-		this.bgColor = "transparent";
+		this.bgColor = this.highlightColor;
 	}
 
 	@HostListener('mouseleave') onMouseLeave() {
 		// background color change via Renderer2 just like @HostBinding('style.backgroundColor') allows us
 		// this.renderer.setStyle(this.elemRef.nativeElement, "background-color", "lightblue")
-		this.bgColor = "lightblue";
+		this.bgColor = this.defaultColor;
 	}
 }
