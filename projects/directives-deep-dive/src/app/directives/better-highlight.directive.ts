817e7b9e935836846667e46861308257d19c92f0
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-11-29 22:25:02 -0300

    Refactor directive betterHighlight within project directives-deep-dive to use @HostBinding for a more direct access to backgroudColor style property

--- projects/directives-deep-dive/src/app/directives/better-highlight.directive.ts
+++ projects/directives-deep-dive/src/app/directives/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
 	selector: '[appBetterHighlight]'
@@ -7,6 +7,8 @@ export class BetterHighlightDirective {
 
 	constructor(private elemRef: ElementRef, private renderer: Renderer2) { }
 
+	@HostBinding('style.backgroundColor') bgColor: string = "lightblue";
+
 	ngOnInit() {
 		// the forth arg for setStyle method are optional flags like '!important', among others
 		//this.renderer.setStyle(this.elemRef.nativeElement, "background-color", "lightblue")
@@ -14,10 +16,14 @@ export class BetterHighlightDirective {
 	}
 
 	@HostListener('mouseenter') onMouseOver() {
-		this.renderer.setStyle(this.elemRef.nativeElement, "background-color", "lightblue")
+		// background color change via Renderer2 just like @HostBinding('style.backgroundColor') allows us
+		// this.renderer.setStyle(this.elemRef.nativeElement, "background-color", "transparent")
+		this.bgColor = "transparent";
 	}
 
 	@HostListener('mouseleave') onMouseLeave() {
-		this.renderer.setStyle(this.elemRef.nativeElement, "background-color", "transparent")
+		// background color change via Renderer2 just like @HostBinding('style.backgroundColor') allows us
+		// this.renderer.setStyle(this.elemRef.nativeElement, "background-color", "lightblue")
+		this.bgColor = "lightblue";
 	}
 }
