0003102d449a8273bff315b29ee61da49e747d83
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-12-03 18:32:02 -0300

    Add comment in logging service in project services-and-dependency-injection

--- projects/services-and-dependency-injection/src/app/logging/logging.service.ts
+++ projects/services-and-dependency-injection/src/app/logging/logging.service.ts
@@ -1,3 +1,7 @@
+// Though this service works and can be normally used (thus injected elsewhere) without the
+// usage of the decorator @Injectable, it's already recommended by official doc that you
+// always use @Injectable to mark this class. Meanwhile, it's really only mandatory if
+// we need to use an injectable in this service (as happens in AccountsService for example)
 export class LoggingService {
 	logStatusChange(status: string) {
 		console.log("A new status changed, new status: ", status);
