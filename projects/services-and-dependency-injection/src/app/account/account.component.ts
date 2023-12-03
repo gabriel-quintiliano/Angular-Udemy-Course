bfaaf6726b408572997055d9e9275630fa945608
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-12-03 15:41:48 -0300

    Implement use of logging service in components account and new-account in project services-and-dependency-injection

--- projects/services-and-dependency-injection/src/app/account/account.component.ts
+++ projects/services-and-dependency-injection/src/app/account/account.component.ts
@@ -1,18 +1,25 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingService } from '../logging/logging.service';
 
 @Component({
 	selector: 'app-account',
 	templateUrl: './account.component.html',
-	styleUrls: ['./account.component.css']
+	styleUrls: ['./account.component.css'],
+	// this below tells Angular to handle the instanciation of service LoggingService, and in
+	// this case provide a new instance of the service for every new AccountComponent	
+	providers: [LoggingService],
 })
 export class AccountComponent {
 	@Input({ required: true }) account!: { name: string, status: string };
 	@Input({ required: true }) id!: number;
 	@Output() statusChanged = new EventEmitter<{ id: number, newStatus: string }>();
 
+	// this tell Angular to inject the instance from LoggingService in this
+	// `loggingService` property of AccountComponent
+	constructor(private loggingService: LoggingService) { }
 
 	onSetTo(status: string) {
 		this.statusChanged.emit({ id: this.id, newStatus: status });
-		console.log('A server status changed, new status: ' + status);
+		this.loggingService.logStatusChange(status)
 	}
 }
--- projects/services-and-dependency-injection/src/app/new-account/new-account.component.ts
+++ projects/services-and-dependency-injection/src/app/new-account/new-account.component.ts
@@ -1,18 +1,27 @@
+import { LoggingService } from '../logging/logging.service';
 import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-new-account',
-  templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+	selector: 'app-new-account',
+	templateUrl: './new-account.component.html',
+	styleUrls: ['./new-account.component.css'],
+	// this below tells Angular to handle the instanciation of service LoggingService, and in
+	// this case provide a new instance of the service for every new NewAccountComponent	
+	providers: [LoggingService],
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+	@Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
 
-  onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
-  }
+	// this tell Angular to inject the instance from LoggingService in this
+	// `loggingService` property of NewAccountComponent
+	constructor(private loggingService: LoggingService) { }
+
+	onCreateAccount(accountName: string, accountStatus: string) {
+		this.accountAdded.emit({
+			name: accountName,
+			status: accountStatus
+		});
+
+		this.loggingService.logStatusChange(accountStatus);
+	}
 }
