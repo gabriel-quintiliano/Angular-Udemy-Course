f0d4c10bdac5b0649007b034fc2ed21330808b16
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-12-03 18:31:06 -0300

    Refactor accounts service to use logging service in project services-and-dependency-injection

--- projects/services-and-dependency-injection/src/app/accounts/accounts.service.ts
+++ projects/services-and-dependency-injection/src/app/accounts/accounts.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
+import { LoggingService } from '../logging/logging.service';
 
+// This decorator is important to mark that this class can be injected into other classes
+// and also receive injections (without it, the injection of LoggingService wouldn't work)
+@Injectable()
 export class AccountsService {
 
 	accounts = [
@@ -17,11 +21,15 @@ export class AccountsService {
 		}
 	];
 
+	constructor(private loggingService: LoggingService) { }
+
 	addAccount(name: string, status: string) {
 		this.accounts.push({ name, status });
+		this.loggingService.logStatusChange(status);
 	}
 
 	updateStatus(id: number, status: string) {
 		this.accounts[id].status = status;
+		this.loggingService.logStatusChange(status);
 	}
 }
