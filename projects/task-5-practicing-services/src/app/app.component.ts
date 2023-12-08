7ad61e670310565e4a3633dc320437a7ce5033a2
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-12-07 22:27:27 -0300

    Update app component in project task-5-practing-serviced

--- projects/task-5-practicing-services/src/app/app.component.ts
+++ projects/task-5-practicing-services/src/app/app.component.ts
@@ -1,21 +1,17 @@
 import { Component } from '@angular/core';
+import { UsersService } from './services/users.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  activeUsers = ['Max', 'Anna'];
-  inactiveUsers = ['Chris', 'Manu'];
 
-  onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
-  }
+	constructor(private users: UsersService) { }
 
-  onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
-  }
+	ngOnInit() {
+		this.users.loadActiveUsers(['Max', 'Anna'])
+		this.users.loadInactiveUsers(['Chris', 'Manu'])
+	}
 }
