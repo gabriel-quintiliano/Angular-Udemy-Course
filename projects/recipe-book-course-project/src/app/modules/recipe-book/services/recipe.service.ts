f64b379b15b69d8669a281a0e432397c3d53c100
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-12-09 15:08:21 -0300

    Update recipe model in project recipe-book-course-project - add 'ingredients' property

--- projects/recipe-book-course-project/src/app/modules/recipe-book/services/recipe.service.ts
+++ projects/recipe-book-course-project/src/app/modules/recipe-book/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from '../models/recipe.model';
+import { Ingredient } from '../../shopping-list/models/ingredient.model';
 
 @Injectable({
 	providedIn: 'root'
@@ -8,10 +9,42 @@ export class RecipeService {
 	recipeSelected = new EventEmitter<Recipe>();
 
 	private recipes: Recipe[] = [
-		new Recipe('Batatas Hasselback', 'Batatas frtas e recheadas deliciosas', 'https://www.cookipedia.co.uk/wiki/images/8/8f/Hasselback_potatoes_recipe.jpg'),
-		new Recipe('Receita avc', 'Batatas frtas e recheadas deliciosas', 'https://www.cookipedia.co.uk/wiki/images/8/8f/Hasselback_potatoes_recipe.jpg'),
-		new Recipe('Abcate gratinado', 'Batatas frtas e recheadas deliciosas', 'https://www.cookipedia.co.uk/wiki/images/8/8f/Hasselback_potatoes_recipe.jpg'),
-		new Recipe('Banana com xia', 'Batatas frtas e recheadas deliciosas', 'https://www.cookipedia.co.uk/wiki/images/8/8f/Hasselback_potatoes_recipe.jpg')
+		new Recipe(
+			'Batatas Hasselback',
+			'Batatas frtas e recheadas deliciosas',
+			'https://www.cookipedia.co.uk/wiki/images/8/8f/Hasselback_potatoes_recipe.jpg',
+			[
+				new Ingredient('batata', 2, 'un'),
+				new Ingredient('cheese', 200, 'g'),
+			]
+		),
+		new Recipe(
+			'Receita avc',
+			'Batatas frtas e recheadas deliciosas',
+			'https://www.cookipedia.co.uk/wiki/images/8/8f/Hasselback_potatoes_recipe.jpg',
+			[
+				new Ingredient('batata', 2, 'un'),
+				new Ingredient('cheese', 200, 'g'),
+			]
+		),
+		new Recipe(
+			'Abcate gratinado',
+			'Batatas frtas e recheadas deliciosas',
+			'https://www.cookipedia.co.uk/wiki/images/8/8f/Hasselback_potatoes_recipe.jpg',
+			[
+				new Ingredient('batata', 2, 'un'),
+				new Ingredient('cheese', 200, 'g'),
+			]
+		),
+		new Recipe(
+			'Banana com xia',
+			'Batatas frtas e recheadas deliciosas',
+			'https://www.cookipedia.co.uk/wiki/images/8/8f/Hasselback_potatoes_recipe.jpg',
+			[
+				new Ingredient('batata', 2, 'un'),
+				new Ingredient('cheese', 200, 'g'),
+			]
+		)
 	]
 
 	getRecipes() {
