0e1e55ed6a972d24f9edbb72214e3ae5c632d6f6
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-12-09 14:39:04 -0300

    Implement shopping-list service in project recipe-book-course-project

--- projects/recipe-book-course-project/src/app/modules/shopping-list/services/shopping-list.service.ts
+++ projects/recipe-book-course-project/src/app/modules/shopping-list/services/shopping-list.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../models/ingredient.model';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ShoppingListService {
+	ingredientsChanged = new EventEmitter<Ingredient[]>();
+	private ingredients: Ingredient[] = [
+		new Ingredient('batata', 1, 'un'),
+	];
 
-  constructor() { }
+	getIngredients() {
+		// returns a shallow copy of ingredients, thus it's a new Array object, but
+		// it's elements are the same from ingredients (same reference).
+		// A deep copy would be better but this will do for now.
+		return this.ingredients.slice();
+	}
+
+	addIngredient(ingredient: Ingredient) {
+		this.ingredients.push(ingredient);
+		this.ingredientsChanged.emit(this.ingredients);
+	}
+
+	removeIngredient(ingredient: Ingredient) {
+		const ingredientIndex = this.ingredients.indexOf(ingredient)
+
+		// if ingredient is not among ingredients, the return index above will be -1
+		if (ingredientIndex > -1) {
+			this.ingredients.splice(ingredientIndex, 1);
+			this.ingredientsChanged.emit(this.ingredients);
+		}
+	}
+
+	editIngredient<K extends keyof Ingredient>(ingredient: Ingredient, key: K, newValue: Ingredient[K]) {
+		const ingredientIndex = this.ingredients.indexOf(ingredient);
+
+		// if ingredient is not among ingredients, the return index above will be -1
+		if (ingredientIndex > -1) {
+			this.ingredients[ingredientIndex][key] = newValue;
+			this.ingredientsChanged.emit(this.ingredients);
+		}
+	}
 }
