f5c29761ea59d1de859eddb6c2fa8be2803d91c6
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-12-09 00:53:47 -0300

    Implement recipe service in project recipe-book-course-project

--- projects/recipe-book-course-project/src/app/modules/recipe-book/services/recipe.service.ts
+++ projects/recipe-book-course-project/src/app/modules/recipe-book/services/recipe.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class RecipeService {
+	recipeSelected = new EventEmitter<Recipe>();
 
-  constructor() { }
+	private recipes: Recipe[] = [
+		new Recipe('Batatas Hasselback', 'Batatas frtas e recheadas deliciosas', 'https://www.cookipedia.co.uk/wiki/images/8/8f/Hasselback_potatoes_recipe.jpg'),
+		new Recipe('Receita avc', 'Batatas frtas e recheadas deliciosas', 'https://www.cookipedia.co.uk/wiki/images/8/8f/Hasselback_potatoes_recipe.jpg'),
+		new Recipe('Abcate gratinado', 'Batatas frtas e recheadas deliciosas', 'https://www.cookipedia.co.uk/wiki/images/8/8f/Hasselback_potatoes_recipe.jpg'),
+		new Recipe('Banana com xia', 'Batatas frtas e recheadas deliciosas', 'https://www.cookipedia.co.uk/wiki/images/8/8f/Hasselback_potatoes_recipe.jpg')
+	]
+
+	getRecipes() {
+		// the slice method will return a shallow copy of the recipes array, therefor it'll be a new array
+		// but with the element will be exactly the same as in the recipes array (i.e. the reference to the
+		// same objects, so altering these in the copy will reflect on recipes because it's the same object)
+		return this.recipes.slice();
+	}
 }
