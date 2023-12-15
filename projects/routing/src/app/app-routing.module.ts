7f4e5a8f458ae83f34eba7b3a94d6a2a4be8d052
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-12-14 22:06:21 -0300

    Outsource routes from app.module to app-routing.module in project routing

---
+++ projects/routing/src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { UsersComponent } from "./users/users.component";
+import { UserComponent } from "./users/user/user.component";
+import { ServersComponent } from "./servers/servers.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+
+const appRoutes: Routes = [
+	// don't use '/' in path property here or your routes won't work
+	{
+		path: '', component: HomeComponent
+	},
+	{
+		path: 'users', component: UsersComponent, children: [
+			{ path: ':id/:name', component: UserComponent },
+		]
+	},
+	{
+		path: 'servers', component: ServersComponent, children: [
+			{ path: ':id', component: ServerComponent },
+			{ path: ':id/edit', component: EditServerComponent },
+		]
+	},
+	{ path: 'not-found', component: NotFoundComponent },
+	// redirectTo follows the rules for usage of '/', '../' and '' related
+	// to relative and absolute path routes
+	{ path: '**', redirectTo: '/not-found' }
+	// if you ever use redirectTo with path route '', see notes on pathMatch in Angular documentation
+	// in this scenario it's better to use pathMatch: 'full' (default value is 'prefix')
+]
+
+@NgModule({
+	imports: [RouterModule.forRoot(appRoutes)],
+	exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- projects/routing/src/app/app.module.ts
+++ projects/routing/src/app/app.module.ts
@@ -10,29 +10,8 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-
-const routes: Routes = [
-	// don't use '/' in path property here or your routes won't work
-	{
-		path: '', component: HomeComponent
-	},
-	{
-		path: 'users', component: UsersComponent, children: [
-			{ path: ':id/:name', component: UserComponent },
-		]
-	},
-	{
-		path: 'servers', component: ServersComponent, children: [
-			{ path: ':id', component: ServerComponent },
-			{ path: ':id/edit', component: EditServerComponent },
-		]
-	},
-	{ path: 'not-found', component: NotFoundComponent },
-	{ path: '**', redirectTo: '/not-found' }
-];
 @NgModule({
 	declarations: [
 		AppComponent,
@@ -44,9 +23,9 @@ const routes: Routes = [
 		ServerComponent
 	],
 	imports: [
-		RouterModule.forRoot(routes),
 		BrowserModule,
-		FormsModule
+		FormsModule,
+		AppRoutingModule
 	],
 	providers: [ServersService],
 	bootstrap: [AppComponent]
