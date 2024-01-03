9c577502b94037c741dba9414230147c07438222
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-01-02 21:03:02 -0300

    Update onUpdateServer method in edit-server component and add property changesSaved in project routing

--- projects/routing/src/app/servers/edit-server/edit-server.component.ts
+++ projects/routing/src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
 	selector: 'app-edit-server',
@@ -13,10 +13,12 @@ export class EditServerComponent implements OnInit {
 	serverName!: string;
 	serverStatus!: string;
 	allowEdit = false;
+    changesSaved = false;
 
 	constructor(
 		private serversService: ServersService,
-		private route: ActivatedRoute
+		private route: ActivatedRoute,
+        private router: Router,
 	) { }
 
 	ngOnInit() {
@@ -43,6 +45,8 @@ export class EditServerComponent implements OnInit {
 
 	onUpdateServer() {
 		this.serversService.updateServer(this.server.id, { name: this.serverName, status: this.serverStatus });
+        this.changesSaved = true;
+        this.router.navigate(["../"], {relativeTo: this.route})
 	}
 
 }
