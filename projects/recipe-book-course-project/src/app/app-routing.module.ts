44402c5ea25a440dd58f5fecf39b912a8550b50f
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-01-07 20:15:01 -0300

    project: recipe-book-course-project - Update AppRoutingModule with child routes

--- projects/recipe-book-course-project/src/app/app-routing.module.ts
+++ projects/recipe-book-course-project/src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipesComponent } from './components/recipes/recipes.component';
 import { ShoppingListComponent } from './modules/shopping-list/components/shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from './modules/recipe-book/components/recipe-detail/recipe-detail.component';
+import { PlaceholderComponent } from './modules/common-custom-utilities/placeholder/placeholder.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipes', component: RecipesComponent },
+    { path: 'recipes', component: RecipesComponent, children: [
+        { path: '', component: PlaceholderComponent, 
+          data: { message: "Click on a recipe to have its details displayed here" }
+        },
+        { path: ':recipe-id', component: RecipeDetailComponent }] },
     { path: 'shoppingList', component: ShoppingListComponent }
 ];
 
