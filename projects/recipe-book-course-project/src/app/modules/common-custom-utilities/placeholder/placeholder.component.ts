da6ed4e412cda89969f6d3f0e1d486cebfc7a194
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-01-07 19:05:51 -0300

    project: recipe-book-course-project - Refactor PlaceholderComponent so that it also be loaded via routing

--- projects/recipe-book-course-project/src/app/modules/common-custom-utilities/placeholder/placeholder.component.ts
+++ projects/recipe-book-course-project/src/app/modules/common-custom-utilities/placeholder/placeholder.component.ts
@@ -1,11 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
 	selector: 'app-placeholder',
 	templateUrl: './placeholder.component.html',
 	styleUrls: ['./placeholder.component.css']
 })
-export class PlaceholderComponent {
+export class PlaceholderComponent implements OnInit {
 	@Input() fillSpace: boolean = true;
+    messageFromRoute!: string;
+
+    constructor(private route: ActivatedRoute) {}
+
+    ngOnInit(): void {
+        this.messageFromRoute = this.route.snapshot.data['message'];
+
+        const fillspaceRouteData = this.route.snapshot.data['fillspace']
+        
+        if (fillspaceRouteData) {
+            this.fillSpace = Boolean(fillspaceRouteData);
+        }
+    }
 	
+    // About fillspace:
+    // The default value is `true`, if it is defined via input, then we'll use that value
+    // if it is defined by the route, then this value will be used regardless
 }
