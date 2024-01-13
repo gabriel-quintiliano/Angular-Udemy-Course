978161e777f520f26de8f4282547c03e70b9a0fc
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-01-13 20:53:16 -0300

    project: understanding-observables - Implement usage of rxjs `interval` operator (function) to create an observable in HomeComponent

--- projects/understanding-observables/src/app/home/home.component.ts
+++ projects/understanding-observables/src/app/home/home.component.ts
@@ -6,13 +6,27 @@ import { Subscription, interval } from 'rxjs';
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
     private firstObsSubscription!: Subscription;
 
     constructor() {}
 
     ngOnInit() {
+        // the .subscription method returns a Subscription, which will latter on need
+        // so that we can unsubscribe from that and prevent any "memory leaks" via
+        // assynchronous that would keep being executed forever if there was no unsub
+        // and there would eventually be the creation of multiple subscriptions as the
+        // component kept being destroied and recreated
         
+        this.firstObsSubscription = interval(1000).subscribe((count) => {
+              console.log(count)
+        })
+    }
+  
+    ngOnDestroy() {
+        // It's super important not to forget to unsubscribe from subscription
+        // created earlier on throughout the component's lifecycle
+        this.firstObsSubscription.unsubscribe()
     }
 }
