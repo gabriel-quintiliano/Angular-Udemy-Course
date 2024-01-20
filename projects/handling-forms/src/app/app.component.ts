9df615644ef9efa74131b73f946faff4bfe74014
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-01-19 22:57:48 -0300

    project: handling-forms - Set up basic structure of a template-driven form in AppComponent

--- projects/handling-forms/src/app/app.component.ts
+++ projects/handling-forms/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
+    suggestUserName() {
+        const suggestedName = 'Superuser';
+    }
+
+    onSubmit(f: NgForm) {
+        console.log(f)
+    }
 }
