2682f083deab368efd6d1853dbcc038f1511cb48
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-01-21 11:34:10 -0300

    project: handling-forms - Add a @ViewChild('f') in AppComponent to have access to the NgForm directive in the component's class

--- projects/handling-forms/src/app/app.component.ts
+++ projects/handling-forms/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -8,11 +8,13 @@ import { NgForm } from '@angular/forms';
 })
 
 export class AppComponent {
+    @ViewChild('f') signupForm!: NgForm;
+
     suggestUserName() {
         const suggestedName = 'Superuser';
     }
 
-    onSubmit(f: NgForm) {
-        console.log(f)
+    onSubmit() {
+        console.log(this.signupForm)
     }
 }
