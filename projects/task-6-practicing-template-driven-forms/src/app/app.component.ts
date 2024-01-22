4d409641beb5901bec49b31c5b57dce4bd6f9225
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-01-22 20:00:42 -0300

    project: task-6-practicing-template-driven-forms - Finish implementing project

--- projects/task-6-practicing-template-driven-forms/src/app/app.component.ts
+++ projects/task-6-practicing-template-driven-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    @ViewChild('f') subForm!: NgForm;
 
+    onSubmit() {
+        console.log(this.subForm)
+    }
 }
