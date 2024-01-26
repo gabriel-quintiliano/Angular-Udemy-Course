b529562d6965217fd98249b9bc31a61fcc2c04bf
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-01-25 22:57:35 -0300

    project: handling-reactive-forms - Implement validators for form controls of signupForm in AppComponent and also update the corresponding form in template

--- projects/handling-reactive-forms/src/app/app.component.ts
+++ projects/handling-reactive-forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
@@ -18,14 +18,24 @@ export class AppComponent implements OnInit {
          * wrapped in "" as this will make sure that during minification of the code,
          * these stay the same and don't get messed up (leading to runtime errors).
          * If you don't do that, you should be good and everything work just fine, but
-         * there is apparently a chance for this kind of error to happen. */
-
+         * there is apparently a chance for this kind of error to happen.
+         * 
+         * Regarding the FormControl constructor:
+         * the 1st arg will be the initial control value (null = no initial value) 
+         * the 2nd arg [optional] are the validators the control should run for the value
+         * the 3rd arg [optional] are async validators
+         * 
+         * visit https://netbasal.com/angular-reactive-forms-tips-and-tricks-bb0c85400b58
+         * for some really cool and useful tips on reactive forms
+         */
         this.signupForm = new FormGroup({
-            'username': new FormControl(null), // use `null` if you don't want the FormControl to have an initial value
-            'email': new FormControl(null),
-            'gender': new FormControl('male')  // eventhough in the template `gender` is a radio input, in the end is
-                                               // just another normal FormControl (as described bellow), which will have
-                                               // the initial value of 'male'
+            'username': new FormControl(null, Validators.required), // don't call the method, just pass the reference
+            'email': new FormControl(null, [Validators.required, Validators.email]), // use an array to pass multiple validators
+            'gender': new FormControl('male')
         })
+    }
+
+    onSubmit() {
+        console.log(this.signupForm)
     }
 }
