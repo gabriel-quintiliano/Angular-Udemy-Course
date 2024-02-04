49aab6e16d5d0f98e66467be57f8b41cdbfcc966
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-04 17:15:32 -0300

    project: recipe-book-course-project - Refactor ShoppingListEditComponent: Create the 'ingredientsForm' reactive form and discarded the @ViewChild approach to deal with ingredients data

--- projects/recipe-book-course-project/src/app/modules/shopping-list/components/shopping-list-edit/shopping-list-edit.component.ts
+++ projects/recipe-book-course-project/src/app/modules/shopping-list/components/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, ViewChild, Renderer2 } from "@angular/core";
+import { Component, Renderer2 } from "@angular/core";
 import { Ingredient, UnitOfMeasureUnion, unitsOfMeasure } from "../../models/ingredient.model";
 import { ShoppingListService } from "../../services/shopping-list.service";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
 	selector: 'app-shopping-list-edit',
@@ -11,21 +12,30 @@ import { ShoppingListService } from "../../services/shopping-list.service";
 export class ShoppingListEditComponent {
 	unitsOfMeasure = unitsOfMeasure; // so that I can use this imported object in the component's template
 
-	@ViewChild('nameInput') nameInputElem!: ElementRef<HTMLInputElement>;
-	@ViewChild('amountInput') amountInputElem!: ElementRef<HTMLInputElement>;
-	@ViewChild('unitOfMeasureSelect') unitOfMeasureSelectElem!: ElementRef<HTMLSelectElement>;
+    ingredientForm = new FormGroup({
+        name: new FormControl('', {nonNullable: true, validators: Validators.required}),
+        amount: new FormControl(1, {nonNullable: true, validators: Validators.required}),
+        unitOfMeasure: new FormControl<UnitOfMeasureUnion>('un', {nonNullable: true, validators: Validators.required})
+    })
 
-	constructor(private renderer: Renderer2, private slService: ShoppingListService) { }
+    // The same FormGroup above could be created using the NonNullableFormBuilder as:
+    // ingredientForm2 = this.nnfb.group({
+    //     name: ['', Validators.required],
+    //     amount: [1, Validators.required],
+    //     unitOfMeasure: this.nnfb.control<UnitOfMeasureUnion>('un', Validators.required)
+    // })
+    //
+    // don't forget to inject or instanciate the NonNullableFormBuilder class as nnfb (or any other identifier)
+
+
+	constructor(private slService: ShoppingListService) { }
 
 	onIngredientAdded() {
 
-		const ingName = this.nameInputElem.nativeElement.value;
-		const ingAmount = Number(this.amountInputElem.nativeElement.value);
-		const ingUnitOfMeasure = this.unitOfMeasureSelectElem.nativeElement.selectedOptions[0].textContent! as UnitOfMeasureUnion;
+		const ingName = this.ingredientForm.get('name')!.value;
+		const ingAmount = this.ingredientForm.get('amount')!.value;
+		const ingUnitOfMeasure = this.ingredientForm.get('unitOfMeasure')!.value;
 
 		this.slService.addIngredient(new Ingredient(ingName, ingAmount, ingUnitOfMeasure));
-
-		this.renderer.setProperty(this.nameInputElem.nativeElement, 'value', ''); // reset name input
-		this.renderer.setProperty(this.amountInputElem.nativeElement, 'value', ''); // reset name input
 	}
 }
