f795fa46c5fee22fbcc7eeb3d3568d0e3472a815
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-09 23:47:17 -0300

    project: recipe-book-course-project - Add interaction with 'recipesChanged' subject from RecipeService to update recipes in RecipeListComponent from RecipeBookModule

--- projects/recipe-book-course-project/src/app/modules/recipe-book/components/recipe-list/recipe-list.component.ts
+++ projects/recipe-book-course-project/src/app/modules/recipe-book/components/recipe-list/recipe-list.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Recipe } from '../../models/recipe.model';
 import { RecipeService } from '../../services/recipe.service';
+import { Subscription } from 'rxjs';
 
 @Component({
 	selector: 'app-recipe-list',
 	templateUrl: './recipe-list.component.html',
 	styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit, OnDestroy {
 	recipes!: Recipe[];
+    subscription!: Subscription;
 
 	constructor(private recipeService: RecipeService) { }
 
 	ngOnInit() {
 		this.recipes = this.recipeService.getRecipes();
+
+        // recipesChanged subject will me sure our recipes array is always up to date
+        this.subscription = this.recipeService.recipesChanged.subscribe(updatedRecipes => {
+            this.recipes = updatedRecipes;
+        })
 	}
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe()
+    }
 }
