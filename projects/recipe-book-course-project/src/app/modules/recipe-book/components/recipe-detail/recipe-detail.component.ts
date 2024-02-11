470eab9dba64fd145644a3b4478ca92f0b473521
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-10 23:42:06 -0300

    project: recipe-book-course-project - Create 'onDeleteRecipe' method and accordingly update template of RecipeDetailComponent from RecipeBookModule

--- projects/recipe-book-course-project/src/app/modules/recipe-book/components/recipe-detail/recipe-detail.component.ts
+++ projects/recipe-book-course-project/src/app/modules/recipe-book/components/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../../models/recipe.model'
 import { RecipeService } from '../../services/recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
 	selector: 'app-recipe-detail',
@@ -13,7 +13,8 @@ export class RecipeDetailComponent {
     recipeId!: number;
 
     constructor( private recipeService: RecipeService,
-                 private route: ActivatedRoute ) {}
+                 private route: ActivatedRoute,
+                 private router: Router ) {}
 
     ngOnInit() {
         // Meanwhile it's this component is being loaded by a route with no guards,
@@ -27,5 +28,28 @@ export class RecipeDetailComponent {
 
     onAddToShoppingList() {
         this.recipeService.addToShoppingList(this.recipe.ingredients)
+    }
+
+    onDeleteRecipe() {
+        this.recipeService.deleteRecipe(this.recipeId);
+
+        // So from this moment on, as a recipe has been deleted, this means the recipes array
+        // within RecipeService has changed (as well as the RecipeListComponent's template),
+        // so for example if we have delete the recipe with id 1, now the recipe that previouly
+        // had id 2 is now recipe with id 1, thus, we better reload the current url, which will
+        // run all the route guards again, to make sure the most up to date data is being displayed.
+        //
+        // To do that, we gotta leave the current route just to get back to it, otherwise Angular
+        // won't actually reload that (for example, `this.router.navigate(['./'], {relativeTo: this.route})`
+        // wouldn't work, because Angular won't just reload this route that is already loaded)
+
+        const currentUrl = this.router.url;
+
+        // Silently navigates to root just to get back to the current route again. And doing this,
+        // the `IsValidRecipeIdGuard` will take care of the recipe-id and figure out if it still
+        // represents a recipe or if it's out of the bounds the recipes array from RecipeService.
+        this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+          this.router.navigateByUrl(currentUrl);
+        });
     }
 }
