2502a55ca40a466831cfdca30cbbb827f3f49f44
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-12 00:08:39 -0300

    project: using-pipes-to-transform-output - Update FilterPipe to {pure: false}

--- projects/using-pipes-to-transform-output/src/app/pipes/filter.pipe.ts
+++ projects/using-pipes-to-transform-output/src/app/pipes/filter.pipe.ts
@@ -1,10 +1,33 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+/* The `pure: false` property of a pipe needs to be handled extremely carefully
+ * because it will make the pipe run its `transform` method for each and every
+ * change that happens in the component, what can be pretty heavy in some scenarios
+ * and waste a bunch of memory creating performance issues.
+ * 
+ * By default, every pipe implicitly has `pure: true` which means the pipe will
+ * only run `transform` when one of its parameters value change. You may think of
+ * it as: if a pipe is pure (default), it only cares about changes that can possibly
+ * alter the return value of its `transform` method. On the other hand, if a pipe
+ * is impure it cares about every change happened in the component in which it was
+ * used, even those which won't alter the return value of `transform` 
+ * 
+ * When to use `{pure: false}`?
+ * 
+ * It is necessary when the pipe takes an Object (or Array - which is also an Object)
+ * as argument, which may have internal values altered in runtime. In this case, as
+ * all kinds of Objects are reference types, no matter what happens, the value the
+ * pipe has (the address of the Object) won't change, thus, `transform` wouldn't be
+ * triggered to run again because of internal changes. */
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
+
     transform(servers: Server[], filterString: string, filterProp: keyof Server): Server[] {
+        console.log('New change detected, FilterPipe (inpure) just ran!')
+
         // if `filteredString` is empty (''), no filtering will happen
         if (!filterString) {
             return servers;
@@ -22,6 +45,7 @@ export class FilterPipe implements PipeTransform {
 
         return filteredServers;
     }
+
 }
 
 type Server = {
