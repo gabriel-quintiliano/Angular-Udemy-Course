9aa0b3e807ab4f53d1e2d9cfd6d1cd5d86934b8d
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-16 00:46:38 -0300

    project: making-http-requests - Update 'onCreatePost' method to also update 'loadedPosts' property right away in AppComponent

--- projects/making-http-requests/src/app/app.component.ts
+++ projects/making-http-requests/src/app/app.component.ts
@@ -18,7 +18,22 @@ export class AppComponent implements OnInit {
     }
 
     onCreatePost(postData: PostData) {
-        this.postsService.createAndStorePost(postData);
+        /* What's the logic bellow?
+         *
+         * As this is very simple app, and we see that by the initialization of this component
+         * all posts from db are fetched (via `this._fetchPosts()`) and we also know that
+         * AppComponent is the only via which a post can be added, we can, at the same time we're
+         * creating and sending the new post to db, safely add this same post to the `loadedPost`
+         * property, which will trigger the updating of the template to show this newly added post
+         * 
+         * Maybe a more responsable (though a bit less efficient) way to do the same would be to
+         * use the post's id returned by the Observable returned by <HttpClient>.get() method within
+         * <PostsService>.createAndStorePost() to fetch this single post with this id */
+        
+        this.postsService.createAndStorePost(postData)
+        .subscribe(responseData => {
+            this.loadedPosts.push({id: responseData.name, ...postData});
+        });
     }
   
     onFetchPosts() {
