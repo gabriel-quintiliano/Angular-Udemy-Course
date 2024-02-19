9939e2889c1e75eea6e7b54b317479493b5aa4c9
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-19 11:42:56 -0300

    project: making-http-requests - Create AuthInterceptor and registered it as a HTTP_INTERCEPTOR provider in AppModule

--- projects/making-http-requests/src/app/app.module.ts
+++ projects/making-http-requests/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AuthInterceptor } from './services/auth.interceptor';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, FormsModule, HttpClientModule],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true
+        }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ projects/making-http-requests/src/app/services/auth.interceptor.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpEventType } from '@angular/common/http';
+import { Observable, map, tap } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+    constructor() {}
+
+    intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+        console.log('method = ', request.method);
+        return next.handle(request);
+    }
+}
+
+/* NOTE: This is an interceptor (out of many your application can have), Angular itself
+ * will manage the calling of the `intercept` method of your custom interceptors and pass
+ * the HttpRequest as the `request` param and inject a HttpHandler as the `next` param,
+ * then you deal with the request (generally you'll transform it in some way).
+ * 
+ * The calling of `next.handle(<transformedRequest>)` will pass the <transformedRequest>
+ * for the next interceptor to handle (remember, `next` is a HttpHandler object), and 
+ * finally, when there are no more interceptors, a last Angular handler (of class HttpBackend)
+ * will send the resquest will all the transformations to the server/backend, then, from
+ * that the Observable of HttpEvents related will be returned going backwards this chain
+ * of handlers and interceptors.
+ * 
+ * The order each interceptors will intercept each is request is based on the order these
+ * were provided in AppModule (or app.config.ts) provider's array.
+ * 
+ * Each interceptor as described above will also be able to deal with the response (and
+ * other HttpEvents), to summarize, the request goes forward the chain of interceptors
+ * in the order they were registered and the corresponding HttpEvents comes backwards
+ * this same chain (and are finally delivered to the HttpClient method which sent the
+ * request initially). */
