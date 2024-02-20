132edfa049a0d76640860cccbcdff34b95bd6440
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-20 17:54:11 -0300

    project: recipe-book-course-project - Create 'loginMode' property and 'onSwitchMode' method, and update template of AuthComponent

--- projects/recipe-book-course-project/src/app/authentication/components/auth/auth.component.ts
+++ projects/recipe-book-course-project/src/app/authentication/components/auth/auth.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+    selector: 'app-auth',
+    templateUrl: './auth.component.html',
+    styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
+    loginMode = true;
 
+    onSwitchMode() {
+        // reverses the previously stored boolean value;
+        this.loginMode = !this.loginMode;
+    }
 }
