45beefb55d9364e9b2e5af1ce40e8e2fc733d5f3
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-20 18:29:37 -0300

    project: recipe-book-course-project - Import ReactiveFormsModule to AppModule

--- projects/recipe-book-course-project/src/app/app.module.ts
+++ projects/recipe-book-course-project/src/app/app.module.ts
@@ -1,15 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonCustomUtilitiesModule } from './modules/common-custom-utilities/common-custom-utilities.module';
 import { RecipeBookModule } from './modules/recipe-book/recipe-book.module';
 import { ShoppingListModule } from './modules/shopping-list/shopping-list.module';
-import { CommonCustomUtilitiesModule } from './modules/common-custom-utilities/common-custom-utilities.module';
-import { HttpClientModule } from '@angular/common/http';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './authentication/components/auth/auth.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
-import { AppRoutingModule } from './app-routing.module';
-import { AuthComponent } from './authentication/components/auth/auth.component';
 
 @NgModule({
 	declarations: [
@@ -24,7 +25,8 @@ import { AuthComponent } from './authentication/components/auth/auth.component';
 		RecipeBookModule,
 		ShoppingListModule,
 		CommonCustomUtilitiesModule,
-        HttpClientModule
+        HttpClientModule,
+        ReactiveFormsModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
