689847cf2eaa1b238b502d0e2c335ac23a1b440e
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-20 18:30:40 -0300

    project: recipe-book-course-project - Create 'userDataForm' reactive form and 'onSubmit' method, and update template of AuthComponent accordingly

--- projects/recipe-book-course-project/src/app/authentication/components/auth/auth.component.ts
+++ projects/recipe-book-course-project/src/app/authentication/components/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-auth',
@@ -7,9 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AuthComponent {
     loginMode = true;
+    userDataForm = this.nnfb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+    })
+
+    constructor(private nnfb: NonNullableFormBuilder) { }
 
     onSwitchMode() {
         // reverses the previously stored boolean value;
         this.loginMode = !this.loginMode;
+    }
+
+    onSubmit() {
+        if (!this.userDataForm.valid) return
+        console.log(this.userDataForm);
     }
 }
