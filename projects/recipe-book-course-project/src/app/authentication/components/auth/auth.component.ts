34a001953802dff7b86581537163fe0c34751573
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-20 23:21:41 -0300

    project: recipe-book-course-project - Update 'onSubmit' method in AuthComponent to use AuthService to signup a new user

--- projects/recipe-book-course-project/src/app/authentication/components/auth/auth.component.ts
+++ projects/recipe-book-course-project/src/app/authentication/components/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
     selector: 'app-auth',
@@ -13,7 +14,8 @@ export class AuthComponent {
         password: ['', [Validators.required, Validators.minLength(6)]],
     })
 
-    constructor(private nnfb: NonNullableFormBuilder) { }
+    constructor(private nnfb: NonNullableFormBuilder,
+                private authService: AuthService) { }
 
     onSwitchMode() {
         // reverses the previously stored boolean value;
@@ -22,6 +24,25 @@ export class AuthComponent {
 
     onSubmit() {
         if (!this.userDataForm.valid) return
-        console.log(this.userDataForm);
+
+        const email = this.userDataForm.get('email')!.value;
+        const password = this.userDataForm.get('password')!.value;
+
+        if (this.loginMode) {
+            // A sign in method will be implemented later on
+        } else {
+            // If the user is not in login mode, then they're signing up
+            this.authService.signup(email, password)
+            .subscribe({
+                next: res => {
+                    console.log("response from signup: ", res)
+                },
+                error: err => {
+                    console.log(err)
+                }
+            });
+        }
+
+        this.userDataForm.reset();
     }
 }
