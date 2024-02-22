f442cffca86e38627453da7f9538b1a126301f36
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-22 18:46:29 -0300

    project: recipe-book-course-project - Delete FirebaseHttpErrorResponse in folder authentication

--- projects/recipe-book-course-project/src/app/authentication/models/firebase-http-error-response.model.ts
+++ projects/recipe-book-course-project/src/app/authentication/models/firebase-http-error-response.model.ts
@@ -1,26 +1,15 @@
 import { HttpErrorResponse } from "@angular/common/http";
 
-export interface FirebaseHttpErrorResponse extends HttpErrorResponse {
+export interface FirebaseHttpErrorResponseForAuthenticationAPI extends HttpErrorResponse {
     error: {
-        error: CustomFirebaseError
+        error: CustomFirebaseAuthError
     }
 };
 
-type CustomFirebaseError = {
+type CustomFirebaseAuthError = {
     readonly code: number,
-    readonly details?: readonly _CustomFirebaseErrorDetails[],
     readonly errors: readonly _CustomFirebaseErrorErrors[],
     readonly message: string,
-    readonly status?: string,
-}
-
-type _CustomFirebaseErrorDetails = {
-    readonly "@type": string,
-    readonly domain: string,
-    readonly metadata: {
-        service: string
-    },
-    readonly reason: string,
 }
 
 type _CustomFirebaseErrorErrors = {
