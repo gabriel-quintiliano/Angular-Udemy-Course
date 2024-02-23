61b9fadffaaee0f215724a5b727cbade27487620
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-22 21:37:13 -0300

    project: recipe-book-course-project - Update CustomAuthErrorMessage with new error codes

--- projects/recipe-book-course-project/src/app/authentication/models/custom-auth-error-message.model.ts
+++ projects/recipe-book-course-project/src/app/authentication/models/custom-auth-error-message.model.ts
@@ -5,8 +5,10 @@ export const CustomAuthErrorMessage = {
     USER_DISABLED: "This account has been disabled.",
     TOKEN_EXPIRED: "Session expired, you must login again.",
     USER_NOT_FOUND: "This user was not found.",
+    EMAIL_NOT_FOUND: "Email or password is invalid.",
     INVALID_REFRESH_TOKEN: "Invalid refresh token provided.",
     INVALID_GRANT_TYPE: "Invalid grant type provided",
     INVALID_LOGIN_CREDENTIALS: "Email or password is invalid.",
+    INVALID_PASSWORD: "Email or password is invalid.",
     MISSING_REFRESH_TOKEN: "No refresh token provided",
 } as const;
