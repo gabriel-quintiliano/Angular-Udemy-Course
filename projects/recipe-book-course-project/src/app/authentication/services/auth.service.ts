6e13474f90779963706b85062f21f9fb39a5c0ce
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-22 21:24:28 -0300

    project: recipe-book-course-project - Outsource error handling logic from 'signup' and 'login' methods to new 'handleError' private method in AuthService

--- projects/recipe-book-course-project/src/app/authentication/services/auth.service.ts
+++ projects/recipe-book-course-project/src/app/authentication/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 import { AuthenticationResponseBody } from '../models/authentication-response-body.model';
 import { CustomAuthErrorMessage } from '../models/custom-auth-error-message.model';
 
@@ -15,55 +15,44 @@ export class AuthService {
         return this.http.post<AuthenticationResponseBody>(
             'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCmPLklaTyYdUsO2I-NMhDT8f39OW_YbWk',
             { email, password, 'returnSecureToken': true }
-        ).pipe(catchError((errorRes: HttpErrorResponse) => {
-            /* This pipe will run only for errors, and in the end will forward only a custom
-             * error message for the subscriber to ocasionally handle */
-
-            // Only Firebase http errors will have an error property and inside that another error
-            // property, this, if the captured error doesn't have this properties, a standard message
-            // is thrown
-            if (!errorRes.error || !errorRes.error.error) {
-                throw "An unknown error has occurred!";
-            }
-
-            const curErrorMessage = errorRes.error.error.message;
-            // Checks if the current error status has a custom message set up in CustomAuthErrorMessage
-            if (CustomAuthErrorMessage[curErrorMessage as keyof typeof CustomAuthErrorMessage] != undefined) {
-
-                // As the current error message is known by CustomAuthErrorMessage, we can directly
-                // access the custom message as the current message is a key in CustomAuthErrorMessage.
-                throw CustomAuthErrorMessage[curErrorMessage as keyof typeof CustomAuthErrorMessage];
-
-                // The code above prevents us, for example, from having to write 98 different cases in a
-                // switch statement if this application had 98 different errors and also have to remember
-                // to return each and every file (where there is similar error handling) to add a new case
-                // for any new error implemented in the future;
-            } else {
-                // For errors that yet not described in CustomAuthErrorMessage object:
-                throw `Error: ${errorRes.error.error.message} [status code: ${curErrorMessage}]`;
-            }
-        }));
+        ).pipe(catchError(this.handleError));
     }
 
     login(email: string, password: string) {
         return this.http.post<AuthenticationResponseBody>(
             'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCmPLklaTyYdUsO2I-NMhDT8f39OW_YbWk',
             { email, password, 'returnSecureToken': true }
-        ).pipe(catchError((errorRes: HttpErrorResponse) => {
-            /* This pipe will run only for errors, and in the end will forward only a custom
-             * error message for the subscriber to ocasionally handle */
+        ).pipe(catchError(this.handleError));
+    }
 
-            if (!errorRes.error || !errorRes.error.error) {
-                throw "An unknown error has occurred!";
-            }
+    // The return type annotation is only necessary because the `catchError` operator expects
+    // a callback with this signature...
+    private handleError(errorRes: HttpErrorResponse): Observable<AuthenticationResponseBody> {
+        /* This pipe will run only for errors, and in the end will forward only a custom
+         * error message for the subscriber to ocasionally handle */
 
-            const curErrorMessage = errorRes.error.error.message;
+        // Only Firebase http errors will have an error property and inside that another error
+        // property, this, if the captured error doesn't have this properties, a standard message
+        // is thrown
+        if (!errorRes.error || !errorRes.error.error) {
+            throw "An unknown error has occurred!";
+        }
 
-            if (CustomAuthErrorMessage[curErrorMessage as keyof typeof CustomAuthErrorMessage] != undefined) {
-                throw CustomAuthErrorMessage[curErrorMessage as keyof typeof CustomAuthErrorMessage];
-            } else {
-                throw `Error: ${errorRes.error.error.message} [status code: ${curErrorMessage}]`;
-            }
-        }));
+        const curErrorMessage = errorRes.error.error.message;
+        // Checks if the current error status has a custom message set up in CustomAuthErrorMessage
+        if (CustomAuthErrorMessage[curErrorMessage as keyof typeof CustomAuthErrorMessage] != undefined) {
+
+            // As the current error message is known by CustomAuthErrorMessage, we can directly
+            // access the custom message as the current message is a key in CustomAuthErrorMessage.
+            throw CustomAuthErrorMessage[curErrorMessage as keyof typeof CustomAuthErrorMessage];
+
+            // The code above prevents us, for example, from having to write 98 different cases in a
+            // switch statement if this application had 98 different errors and also have to remember
+            // to return each and every file (where there is similar error handling) to add a new case
+            // for any new error implemented in the future;
+        } else {
+            // For errors that yet not described in CustomAuthErrorMessage object:
+            throw `Error: ${errorRes.error.error.message} [status code: ${curErrorMessage}]`;
+        }
     }
 }
