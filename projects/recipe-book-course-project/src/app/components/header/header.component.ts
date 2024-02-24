6314c954c78583dc8810fc1572ebd8193395c5a3
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-23 22:51:32 -0300

    project: recipe-book-course-project - Create properties, methods and update template of HeaderComponent to react to authentication

--- projects/recipe-book-course-project/src/app/components/header/header.component.ts
+++ projects/recipe-book-course-project/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../authentication/services/auth.service';
 import { DataStorageService } from '../../shared/services/data-storage.service';
 
 @Component({
@@ -6,9 +8,31 @@ import { DataStorageService } from '../../shared/services/data-storage.service';
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+    protected authenticated = false;
+    private userSub!: Subscription;
 
-    constructor(private dataStorageService: DataStorageService) {}
+    constructor(
+        private dataStorageService: DataStorageService,
+        private authService: AuthService
+    ) {}
+
+    ngOnInit() {
+        this.userSub = this.authService.user.subscribe((authUser) => {
+            // The core idea is that if we have a authenticated user,
+            // authUser = `User` object, otherwise authUser = `null`
+
+            this.authenticated = !!authUser;
+            // The code above is a shortcut for the code below, it works
+            // because `authenticated` expects a boolean value.
+            //
+            // if (authUser) {
+            //     this.authenticated = true;
+            // } else {
+            //     this.authenticated = false;
+            // }
+        })
+    }
 
     onSaveData() {
         this.dataStorageService.storeRecipes();
@@ -16,5 +40,9 @@ export class HeaderComponent {
 
     onFetchData() {
         this.dataStorageService.fetchRecipes();
+    }
+
+    ngOnDestroy() {
+        this.userSub.unsubscribe()
     }
 }
