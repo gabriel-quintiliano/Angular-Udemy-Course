52562483237f72adfe25214bb8140d2203b0bd30
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-24 21:03:56 -0300

    project: recipe-book-course-project - Implement 'ngOnInit' hook in AppComponent to call <AuthService>.autoLogin()

--- projects/recipe-book-course-project/src/app/app.component.ts
+++ projects/recipe-book-course-project/src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
-import { ContentAvailableToBeShownType, contentAvailableToBeShown } from './models/content.model';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './authentication/services/auth.service';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+    constructor(private authService: AuthService) {}
+
+    ngOnInit() {
+        // As that's the component that loads first, it makes some sense to
+        // put this call here which will auto login if there are valid credentials
+        // stored in local storage.
+        this.authService.autoLogin()
+    }
+}
